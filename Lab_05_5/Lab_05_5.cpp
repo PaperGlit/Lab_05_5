a5323ae16090ee25f3ffa6cbfffbd319b7cb0740
Glit <vpad.lazar.04@gmail.com>
2022-11-04 15:26:29 +0200

    Add project files.

---
+++ Lab_05_5/Lab_05_5.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Pascal1(int i, int j)
+{
+    if (j == 1 || i == j)
+        return 1;
+    return (Pascal1(i - 1, j - 1) + Pascal1(i - 1, j));
+}
+
+void Pascal2(int a, int b, int i)
+{
+    if (b < i - a + 1)
+    {
+        cout << "   ";
+        Pascal2(a ,b + 1, i);
+    }
+}
+
+void Pascal3(int a, int b)
+{
+    if (b <= a)
+    {
+        cout << Pascal1(a, b) << "   ";
+        Pascal3(a, b + 1);
+    }
+}
+
+void Pascal(int a, int i)
+{
+    if (a <= i)
+    {
+        Pascal2(a , 1, i);
+        Pascal3(a, 1);
+        cout << endl;
+        Pascal(a + 1, i);
+    }
+}
+
+int main() 
+{
+    int n;
+
+    cout << "n = "; cin >> n;
+
+    Pascal(1, n);
+
+    return 0;
+}
---
+++ UnitTest_Lab_05_5/UnitTest_Lab_05_5.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_05_5/Lab_05_5.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab055
+{
+	TEST_CLASS(UnitTestLab055)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int b;
+			b = Pascal1(5, 3);
+			Assert::AreEqual(b, 6);
+		}
+	};
+}
